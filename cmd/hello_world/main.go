f85c0c5a7ddd117459c30ce8216f552e3cad6879
Penn <penn11.liu@gmail.com>
2025-05-18 22:56:02 +0800

    Re-add main.go and main_test.go after fixing .gitignore

---
+++ cmd/hello_world/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	log.Println("Starting hello_world application...")
+	fmt.Println("Hello, World!")
+}
---
+++ cmd/hello_world/main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	// This is a simple test to ensure the package builds
+	t.Run("package builds", func(t *testing.T) {
+		// If we get here, the package builds successfully
+	})
+}
