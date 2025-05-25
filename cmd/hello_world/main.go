9ebd509207d0be4804a8b55c739eecf85573657c
Penn <penn11.liu@gmail.com>
2025-05-25 10:55:35 +0800

    Refactor: extract PrintHello and add tested output for coverage

--- cmd/hello_world/main.go
+++ cmd/hello_world/main.go
@@ -7,5 +7,9 @@ import (
 
 func main() {
 	log.Println("Starting hello_world application...")
+	PrintHello()
+}
+
+func PrintHello() {
 	fmt.Println("Hello, World!")
 }
--- cmd/hello_world/main_test.go
+++ cmd/hello_world/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"os"
 	"testing"
 )
 
@@ -9,4 +11,24 @@ func TestMain(t *testing.T) {
 	t.Run("package builds", func(t *testing.T) {
 		// If we get here, the package builds successfully
 	})
+}
+
+func TestPrintHello(t *testing.T) {
+	old := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	PrintHello()
+
+	w.Close()
+	os.Stdout = old
+
+	var buf bytes.Buffer
+	_, _ = buf.ReadFrom(r)
+	output := buf.String()
+
+	expected := "Hello, World!\n"
+	if output != expected {
+		t.Errorf("expected %q, got %q", expected, output)
+	}
 }
